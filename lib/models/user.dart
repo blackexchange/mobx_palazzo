d8c0fb7f4420d587438382fb2840aa1e52fe4e65
Neville <rodneyneville@gmail.com>
2023-03-14 11:00:16 -0300

    User

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,16 +1,39 @@
 enum UserType { ADMIN, MORADOR, PROPRIETARIO, FUNCIONARIO, SUPER, ADMUNIDADE }
 
 class User {
+  late final String id;
   late final String nome;
   late final String email;
   late final String telefone;
-  late final String senha;
+  final String? senha;
   final UserType tipo;
 
   User(
-      {required this.nome,
+      {required this.id,
+      required this.nome,
       required this.email,
       required this.telefone,
-      required this.senha,
+      this.senha,
       this.tipo = UserType.MORADOR});
+
+  factory User.fromJson(String key, Map json) {
+    /* if (!json.keys.toSet().containsAll(['profile'])) {
+      throw Exception();
+    }*/
+
+    return User(
+        id: key,
+        nome: json['nome'],
+        email: json['email'],
+        telefone: json['telefone'],
+        tipo: json['tipo']);
+  }
+
+  Map toJson() => {
+        'id': id,
+        'nome': nome,
+        'email': email,
+        'telefone': telefone,
+        'tipo': tipo
+      };
 }
